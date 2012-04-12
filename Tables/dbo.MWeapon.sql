e4505c8e1c928873d8cd5a6b0d6806591d28d4de
randy <randy.jackson@bodybuilding.com>
2012-04-12 10:50:09 -0600

    Add Columns

--- Tables/dbo.MWeapon.sql
+++ Tables/dbo.MWeapon.sql
@@ -3,7 +3,9 @@ CREATE TABLE [dbo].[MWeapon]
 [MWeaponId] [bigint] NOT NULL,
 [Name] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [Description] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[Dice] [int] NULL CONSTRAINT [DF_MWeapon_Dice] DEFAULT ((6))
+[Dice] [int] NULL CONSTRAINT [DF_MWeapon_Dice] DEFAULT ((6)),
+[CreateDate] [datetime] NULL,
+[CreatedBy] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[MWeapon] ADD CONSTRAINT [PK_MWeapon] PRIMARY KEY CLUSTERED  ([MWeaponId]) ON [PRIMARY]
