7dd57f8ea1d059e9e9240b4b8f4f3bc3e6b57fbc
randy <randy.jackson@bodybuilding.com>
2012-04-12 10:30:32 -0600

    initial Change

---
+++ Tables/dbo.MWeapon.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[MWeapon]
+(
+[MWeaponId] [bigint] NOT NULL,
+[Name] [nvarchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Description] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[Dice] [int] NULL CONSTRAINT [DF_MWeapon_Dice] DEFAULT ((6))
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[MWeapon] ADD CONSTRAINT [PK_MWeapon] PRIMARY KEY CLUSTERED  ([MWeaponId]) ON [PRIMARY]
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [IX_MWeapon] ON [dbo].[MWeapon] ([Name]) ON [PRIMARY]
+GO
