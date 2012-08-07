1575eaa5e07eb2fda995fdee9f2562040a50a72f
randy <randy.jackson@bodybuilding.com>
2012-08-07 15:21:22 -0600

    Add primary key to Alchemy

--- Tables/dbo.Alchemy.sql
+++ Tables/dbo.Alchemy.sql
@@ -1,10 +1,12 @@
 CREATE TABLE [dbo].[Alchemy]
 (
-[AlchemyId] [int] NULL,
+[AlchemyId] [int] NOT NULL,
 [AlchemyName] [nvarchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [AlchemyDescriptionShort] [nvarchar] (255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [ElementID] [int] NULL
 ) ON [PRIMARY]
+ALTER TABLE [dbo].[Alchemy] ADD 
+CONSTRAINT [PK_Alchemy] PRIMARY KEY CLUSTERED  ([AlchemyId]) ON [PRIMARY]
 GO
 ALTER TABLE [dbo].[Alchemy] ADD CONSTRAINT [FK_Alchemy_Element] FOREIGN KEY ([ElementID]) REFERENCES [dbo].[Element] ([ElementID])
 GO
