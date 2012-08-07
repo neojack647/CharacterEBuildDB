9aa860079e1e2791357ec4ffde9c499fbd6645d0
randy <randy.jackson@bodybuilding.com>
2012-08-07 15:23:39 -0600

    Add Data to Alchemy

---
+++ Data/dbo.Alchemy_Data.sql
@@ -0,0 +1,2 @@
+INSERT INTO [dbo].[Alchemy] ([AlchemyId], [AlchemyName], [AlchemyDescriptionShort], [ElementID]) VALUES (1, N'Gold', N'TestLink', 3)
+INSERT INTO [dbo].[Alchemy] ([AlchemyId], [AlchemyName], [AlchemyDescriptionShort], [ElementID]) VALUES (2, N'Silver', N'TestLink', 2)
